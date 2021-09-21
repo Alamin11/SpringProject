7870d0039fe888193e24cf92282bc6db64bb7f21
Mohammad_AlAmin <alamincsecu@gmail.com>
2021-09-21 09:43:58 +0600

    Add MarioGame as spring project

--- gameRunner/src/main/java/com/dayDreamer/gameRunner/GameRunnerApplication.java
+++ gameRunner/src/main/java/com/dayDreamer/gameRunner/GameRunnerApplication.java
@@ -2,12 +2,29 @@ package com.dayDreamer.gameRunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import com.dayDreamer.gameRunner.game.GameRunner;
+import com.dayDreamer.gameRunner.game.MarioGame;
+import com.dayDreamer.gameRunner.game.SuperContraGame;
 
 @SpringBootApplication
 public class GameRunnerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GameRunnerApplication.class, args);
+		ConfigurableApplicationContext context = 
+				SpringApplication.run(GameRunnerApplication.class, args);
+		
+		//GameRunner, MarioGame
+	GameRunner runner = context.getBean(GameRunner.class);
+//		
+//		MarioGame game = new MarioGame();
+//		//SuperContraGame game = new SuperContraGame();
+//
+//		GameRunner runner = new GameRunner(game);
+		
+		
+		runner.run();
 	}
 
 }
